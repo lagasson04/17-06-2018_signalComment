f21021a68da3fa5a796c5b989f0ef022d8af088b
david <david.ravel@yahoo.fr>
2018-06-19 18:12:26 +0200

    modif adminViewPage.php

--- 01_Sans_POO/Project/view/backend/adminViewPage.php
+++ 01_Sans_POO/Project/view/backend/adminViewPage.php
@@ -5,8 +5,15 @@ if (isset($_SESSION['id']) AND isset($_SESSION['login']))
     echo 'Bonjour ' . $_SESSION['login'];
 }
 echo "<br />";
-echo "ok admin"; ?>
+echo "ok admin"; 
+echo "<br />";
+echo $_SESSION['id'];
+echo "<br />";
+echo $_POST['pass'];
+echo "<br />";
+echo $_SESSION['pass'];
 
+?>
 <p><a href="index.php">Accueil</a></p>
 
 <p><form action="index.php?action=out" method="POST">
