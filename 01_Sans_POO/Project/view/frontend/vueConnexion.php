868ccc940ff3e72e603b88a4c8cf52731eb550ef
david <david.ravel@yahoo.fr>
2018-06-19 10:40:42 +0200

    Ajout fichier vueConnexion.php

---
+++ 01_Sans_POO/Project/view/frontend/vueConnexion.php
@@ -0,0 +1,23 @@
+<?php $title = "Connexion"; ?>
+<?php ob_start(); ?>
+<header><h1>Connexion</h1></header>
+<div class="container">
+	<div class="row">
+		<form class="col-lg-offset-3 col-lg-6" action="" method="post">
+			<div class="form-group">
+				<label for="pseudo">Pseudo*</label>
+				<input type="text" class="form-control" name="pseudo" id="pseudo" required="required" placeholder="Votre pseudo">
+			</div>
+			<div class="form-group">
+				<label for="pass">Mot de passe*</label>
+				<input type="password" class="form-control" name="pass" id="pass" required="required" placeholder="Mot de passe">
+			</div>
+			<button type="submit" class="pull-right btn btn-danger" name="envoyer"><span class="glyphicon glyphicon-ok-sign"> </span> Envoyer</button>
+		</form>
+	</div>
+	<div class="row">
+		<p><a href="../../index.php">Retour à l'accueil</a></p>
+	</div>
+</div>			
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
