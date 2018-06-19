b07a74737b56b0eff49469c917d7ad4f393d326b
david <david.ravel@yahoo.fr>
2018-06-19 12:17:25 +0200

    Ajout des fichiers backend

---
+++ 01_Sans_POO/Project/controller/backend.php
@@ -0,0 +1,5 @@
+<?php
+function connectionTest($login, $pass){
+	//si la connexion marche, on envoi vers l'espace d'administration. Sinon on renvoi vers la vue de connexion
+
+}
---
+++ 01_Sans_POO/Project/model/backend.php
@@ -0,0 +1,5 @@
+<?php 
+function getConnection($login, $pass){
+	//on teste si le couple login/pass existe dans la bdd 
+	//si oui on renvoi 1 si ça existe, sinon on renvoi 0
+}
