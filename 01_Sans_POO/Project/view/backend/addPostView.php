39f4f5583d69081a8f0d19c96e3f8a45e32af2c3
david <david.ravel@yahoo.fr>
2018-06-24 23:23:48 +0200

    Ajout fichier addPostView.php

---
+++ 01_Sans_POO/Project/view/backend/addPostView.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<?php $title = 'AddPost'; ?>
+<?php ob_start(); ?>
+<h2>Ajouter un chapitre</h2>
+<div class="comment">
+	<form action="index.php?action=adPost" method="post">
+		<div>
+			<label for="title">Titre</label><br />
+			<input type="text" id="title" name="title" required="required" />
+		</div>
+		<div>
+			<label for="content">Contenu</label><br />
+			<textarea id="content" name="content" required="required"></textarea>
+		</div>
+		<div>
+			<input type="submit" value="Ajouter" />
+		</div>
+	</form>
+</div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
