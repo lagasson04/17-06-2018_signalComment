a2db7679dd3d70698f51c8e9370bbef722a325de
david <david.ravel@yahoo.fr>
2018-06-28 16:02:43 +0200

    Ajout liste des billets sur page admin

--- 01_Sans_POO/Project/view/backend/adminViewPage.php
+++ 01_Sans_POO/Project/view/backend/adminViewPage.php
@@ -1,24 +1,30 @@
 <?php session_start(); ?>
 <?php $title = 'Admin'; ?>
 <?php ob_start(); ?>
+<?php $posts = getPosts();
+while ($data = $posts->fetch())
+{
+?>
 <table class="table table-striped"> 
 	<tr>
-		<th>Titre</th>
-		<th>Contenu</th>
-		<th>Date</th>
-		<th>Action</th>
+		<th style="width:25%;">Titre</th>
+		<th style="width:25%;">Contenu</th>
+		<th style="width:25%;">Date</th>
+		<th style="width:25%;">Action</th>
 	</tr>
 	<tr>
-		<td></td>
-		<td></td>
-		<td></td>
+		<td><?= htmlspecialchars($data['title']) ?></td>
+		<td><?= nl2br(htmlspecialchars($data['content'])) ?></td>
+		<td>le <?= $data['creation_date_fr'] ?></td>
 		<td><form action="index.php?action=addPost" method="post">
 			<input class="btn btn-success" type="submit" value="Ajouter">
-			<a class="btn btn-primary" href="#" role="button">Modifier</a>
-			<a class="btn btn-danger" href="#" role="button">Supprimer</a>
 		</form></td>
 	</tr>
 </table>
+<?php
+}
+$posts->closeCursor();
+?>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('template.php'); ?>
